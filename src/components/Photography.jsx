56588c8f0554ebe8ca14b6b710e4a41a68956cf5
Guy <yaweh23@gmail.com>
2020-12-12 02:10:07 -0800

    photographyComponent

--- src/components/Photography.jsx
+++ src/components/Photography.jsx
@@ -5,7 +5,6 @@ import bigTree from './imgs/bigTree.jpg';
 export function Photography() {
     return (
         //CREATE "Photography" TEXT IMAGE PNG, OVERLAY IT ON CARD, TRANSITION IT ALL IN WITH FADE
-        //Pog
         <div style={{marginBottom:'200px', marginTop:'200px', marginLeft:'45px'}}>
             <Card style={{position:'relative', width: '15rem', boxShadow: '5px 5px 5px black', border:'24px', borderStyle:'round'}}>
             <Card.Img variant="top" src={bigTree}  />
